f6ce9c17e1a388e14eeefffac11b67b9d6f5a66c
Thomas Lopez <lopeztomaa0@gmail.com>
2022-11-24 23:04:43 -0300

    primerEntrega

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { PrimerComponenteComponent } from './primer-componente/primer-componente.component';
+import { ContenidoComponent } from './contenido/contenido.component';
+import { RedesComponent } from './redes/redes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PrimerComponenteComponent
+    PrimerComponenteComponent,
+    ContenidoComponent,
+    RedesComponent
   ],
   imports: [
     BrowserModule
